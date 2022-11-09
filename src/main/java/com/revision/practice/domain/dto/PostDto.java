2ff6c2ce9c3507315083f3b840700618ed284590
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-09 11:38:09 +0900

    feat: Post 엔티티 추가 후 PostDto 에 DTO 를 엔티티로 변환할 수 있게 하는 toEntity() 메서드 추가 완료

--- src/main/java/com/revision/practice/domain/dto/PostDto.java
+++ src/main/java/com/revision/practice/domain/dto/PostDto.java
@@ -1,12 +1,15 @@
 package com.revision.practice.domain.dto;
 
 
+import com.revision.practice.domain.entity.Post;
+
 public class PostDto {
     private Long id;
     private String title;
     private String contents;
+    private String author;
 
-//    public Post toEntity() {
-
-//    }
+    public Post toEntity() {
+        return new Post(this.title, this.contents, this.author);
+    }
 }
---
+++ src/main/java/com/revision/practice/domain/entity/Post.java
@@ -0,0 +1,25 @@
+package com.revision.practice.domain.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="board")
+@Getter
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String contents;
+    private String author;
+
+    public Post(String title, String contents, String author) {
+        this.title = title;
+        this.contents = contents;
+        this.author = author;
+    }
+}
